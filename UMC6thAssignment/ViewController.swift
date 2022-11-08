f919e7ea53f05690c3066ec7a5d657d3c5f1f7e6
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2022-11-09 05:31:20 +0900

    UserDefaults added

--- UMC6thAssignment/ViewController.swift
+++ UMC6thAssignment/ViewController.swift
@@ -27,19 +27,35 @@ class ViewController: UIViewController {
     var fixedMinute = 0
     var fixedSecond = 0
     
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nowDateAndTime()
         pickerFunction()
         
+        testLabel.text = "최근 설정한 시분초는 \(String(UserDefaults.standard.integer(forKey: "hour")))시간 or \(String(UserDefaults.standard.integer(forKey: "min")))분 or \(String(UserDefaults.standard.integer(forKey: "sec")))초 이다"
+
         }
+
+
     
     @IBAction func clickConfirm(_ sender: UIButton) {
 
         
+//        hour = settedTime / 3600
+//        minute = settedTime / 60
+//        second = settedTime
+        
         hour = settedTime / 3600
         minute = settedTime / 60
         second = settedTime
+        
+        UserDefaults.standard.set(hour, forKey: "hour")
+        UserDefaults.standard.set(minute, forKey: "min")
+        UserDefaults.standard.set(second, forKey: "sec")
+        
         
         if switchForDQ == 0{
             // 현재 시각 text로 구하기
@@ -68,9 +84,11 @@ class ViewController: UIViewController {
             let secondQueue = DispatchWorkItem(qos: .userInitiated){
                 var countdown = self.second
                 for _ in stride(from: 0, to: self.second, by: 1){
+
                     DispatchQueue.main.async {
                         self.secondLeft.text = String(countdown)
                         countdown -= 1
+                        
                         if countdown == 0{
                             let alert = UIAlertController(title:"알람이 끝났습니다",
                                 message: "다시 설정하시려면 앱을 재시작해주세요",
@@ -78,9 +96,7 @@ class ViewController: UIViewController {
                             let confirm = UIAlertAction(title: "확인", style: .default, handler: nil)
                             alert.addAction(confirm)
                             self.present(alert,animated: true,completion: nil)
-                            print("???")
                         }
-                        print("for")
                     }
                     usleep(1000000)
                 }
@@ -90,6 +106,7 @@ class ViewController: UIViewController {
             let minuteQueue = DispatchWorkItem(qos: .userInitiated){
                 var countdown = self.minute
                 for _ in stride(from: 0, to: self.minute, by: 1){
+
                     DispatchQueue.main.async {
                         self.minuteLeft.text = String(countdown)
                         countdown -= 1
@@ -102,6 +119,7 @@ class ViewController: UIViewController {
             let hourQueue = DispatchWorkItem(qos: .userInitiated){
                 var countdown = self.hour
                 for _ in stride(from: 0, to: self.hour, by: 1){
+
                     DispatchQueue.main.async {
                         self.hourLeft.text = String(countdown)
                         countdown -= 1
@@ -114,6 +132,10 @@ class ViewController: UIViewController {
             myQueue.async(execute: minuteQueue)
             myQueue.async(execute: hourQueue)
             switchForDQ = 1
+            
+
+            
+            
         }else{
             //1. 경고창 내용 만들기
             let alert = UIAlertController(title:"알람이 이미 실행중입니다.",
@@ -138,7 +160,6 @@ class ViewController: UIViewController {
         let time = formatter.string(from: Date())
         self.nowData.text = time
     }
-    
     
     
     func pickerFunction(){
